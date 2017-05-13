739854f95becfea958e62aa5b27750cd2854557d
rupsajina@gmail.com <rupsajina@gmail.com>
2017-05-13 15:59:57 +0530

    tried touch

--- js/index.js
+++ js/index.js
@@ -88,11 +88,6 @@ canvas.addEventListener('mousemove', function(e) {
   mouse.y = e.pageY - this.offsetTop-h/2;
 }, false);
 
-/* Mouse Capturing Work */
-canvas.addEventListener('touchmove', function(e) {
-  touch.x = e.pageX - this.offsetLeft-w/2;
-  touch.y = e.pageY - this.offsetTop-h/2;
-}, false);
 
 ctx.strokeStyle = "red";
 
@@ -153,22 +148,44 @@ canvas.addEventListener('mousedown', function(e) {
     canvas.addEventListener('mousemove', onPaint, false);
 }, false);
 
-canvas.addEventListener('touchstart', function(e) {
-    ctx.beginPath();
-    ctx.moveTo(touch.x, touch.y);
- 
-    canvas.addEventListener('touchmove', onPaint_t, false);
-}, false);
  
 canvas.addEventListener('mouseup', function() {
     canvas.removeEventListener('mousemove', onPaint, false);
     paint_reflection();
 }, false);
 
-canvas.addEventListener('touchend', function() {
-    canvas.removeEventListener('touchmove', onPaint_t, false);
-    paint_reflection();
+
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+        mousePos = getTouchPos(canvas, e);
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousedown", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
 }, false);
+canvas.addEventListener("touchend", function (e) {
+  var mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousemove", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
+}
  
 var onPaint = function() {
     ctx.lineTo(mouse.x, mouse.y);
